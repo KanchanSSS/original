dfd7fd2b1470ed3294c34f46683a9d63e6c80149
KanchanSSS <kanchanshendre90@gmail.com>
2025-06-06 13:52:54 +0530

    Validation of Input fields and showing the modal.

--- React Practice Code/REFs-DEMO-PROJECT/src/components/NewProject.jsx
+++ React Practice Code/REFs-DEMO-PROJECT/src/components/NewProject.jsx
@@ -1,6 +1,8 @@
 import Input from './Input.jsx';
 import {useRef} from 'react';
+import Modal from './Modal.jsx';
 export default function NewProject({onAdd}){
+    const modal = useRef();
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -10,13 +12,25 @@ export default function NewProject({onAdd}){
         const enteredDueDate = dueDate.current.value;
 
         //Validation..
+        if(enteredTitle === '' || enteredDescription === '' || enteredDueDate === '')
+        {
+            modal.current.open();
+            return;
+        }
         onAdd({
             title : enteredTitle,
             description : enteredDescription,
             dueDate : enteredDueDate
         });
     }
-    return <div className='w-[35rem] mt-16'>
+    return (
+    <>
+    <Modal ref={modal} buttonCaption="Okey">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">Oops... Looks like you forgot to enter a value.</p>    
+        <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
+    </Modal>
+    <div className='w-[35rem] mt-16'>
         <menu className="flex items-center justify-end gap-4 my-4">
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
             <li>
@@ -32,4 +46,6 @@ export default function NewProject({onAdd}){
             <Input type="date" ref={dueDate} label="Due Date" textarea={false}/>
         </div>
     </div>
+    </>
+    );
 }
