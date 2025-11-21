ba95d9128fbf17dd74a7f0fda8cc4d4c3b15790b
KanchanSSS <kanchanshendre90@gmail.com>
2025-11-21 17:42:07 +0530

    angular essentials completed

--- Angular_StartingProject/src/app/user/user.component.ts
+++ Angular_StartingProject/src/app/user/user.component.ts
@@ -1,17 +1,20 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+  imports: [CardComponent]
 })
 export class UserComponent { 
-  @Input({required:true}) id! : string;
-  @Input({required:true}) avatar! :string;
-  @Input({required:true}) name! : string;
+  @Input({required:true}) user! : User;
+  @Input({required:true}) selected! : boolean;
   @Output() select = new EventEmitter();
   //select = output<string>()
   // avatar = input.required<string>();
@@ -21,9 +24,9 @@ export class UserComponent {
   //   return 'assets/users/'+this.avatar();
   // })
   get imagePath(){
-    return 'assets/users/'+this.avatar;
+    return 'assets/users/'+this.user.avatar;
   }
   onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
