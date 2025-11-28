3d9463ea4c0c7e09fc757f886558e9933ccf2cd4
KanchanSSS <kanchanshendre90@gmail.com>
2025-11-28 20:08:58 +0530

    Reusable Component dashboard-item

---
+++ Angular_Module3/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { FormsModule } from '@angular/forms';
+import { TaskComponent } from './tasks/task/task.component';
+import { SharedModule } from './shared/card/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, FormsModule, SharedModule],
+})
+export class AppModule {}
