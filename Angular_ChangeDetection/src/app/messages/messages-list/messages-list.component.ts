8e2228caa1ce072ba335204f2a19a3bf00239bd8
KanchanSSS <kanchanshendre90@gmail.com>
2025-12-09 09:01:29 +0530

    Change Detection with BehaviorSubject and subscription

---
+++ Angular_ChangeDetection/src/app/messages/messages-list/messages-list.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { MessagesService } from '../messages.service';
+
+@Component({
+  selector: 'app-messages-list',
+  standalone: true,
+  templateUrl: './messages-list.component.html',
+  styleUrl: './messages-list.component.css',
+  changeDetection:ChangeDetectionStrategy.OnPush
+})
+export class MessagesListComponent implements OnInit {
+  private messagesService = inject(MessagesService);
+  private cdRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+  messages : string[] = [];
+
+  ngOnInit(){
+    const subscription = this.messagesService.messages$.subscribe((messages) => {
+      this.messages = messages;
+      this.cdRef.markForCheck();
+    });
+
+    this.destroyRef.onDestroy(()=>{
+      subscription.unsubscribe();
+    });
+      
+  }
+  get debugOutput() {
+    console.log('[MessagesList] "debugOutput" binding re-evaluated.');
+    return 'MessagesList Component Debug Output';
+  }
+}
