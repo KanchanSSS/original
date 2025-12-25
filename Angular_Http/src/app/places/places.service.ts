54d4d51fe6b85f43a193540ee19a7e4777381689
KanchanSSS <kanchanshendre90@gmail.com>
2025-12-25 12:58:41 +0530

    Show Error modal on error

--- Angular_Http/src/app/places/places.service.ts
+++ Angular_Http/src/app/places/places.service.ts
@@ -3,14 +3,16 @@ import { inject, Injectable, signal } from '@angular/core';
 import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, tap, throwError } from 'rxjs';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PlacesService {
+export class PlacesService {  
   private userPlaces = signal<Place[]>([]);
   private httpClient = inject(HttpClient);
   loadedUserPlaces = this.userPlaces.asReadonly();
+  private errorService = inject(ErrorService);
 
   loadAvailablePlaces() {
     return this.fetchPlaces(
@@ -29,11 +31,21 @@ export class PlacesService {
   }
 
   addPlaceToUserPlaces(place: Place) {
-    this.userPlaces.update((prevPlaces) => [...prevPlaces, place]);
+    const prevPlaces = this.userPlaces();
+
+    if(!prevPlaces.some(p => p.id === place.id)){
+        this.userPlaces.set([...prevPlaces, place]);
+    }
 
     return this.httpClient.put("http://localhost:3000/user-places", {
       placeId: place.id
-    })
+    }).pipe(
+      catchError((error) => {
+        this.userPlaces.set(prevPlaces);
+        this.errorService.showError("Failed to store places.")
+        return throwError(() =>{"Failed to store places."});
+      })
+    )
   }
 
   removeUserPlace(place: Place) {}
